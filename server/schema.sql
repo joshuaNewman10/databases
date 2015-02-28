68d0bab5f0caa5960e502f15820eb978dcc002d1
Hack Reactor Students <accounts@hackreactor.com>
2015-02-27 21:09:22 -0800

    Refactor schema

--- server/schema.sql
+++ server/schema.sql
@@ -13,24 +13,24 @@ CREATE TABLE messages (
   text VARCHAR(140),
   room_id INT,
   user_id INT,
-  created_at DATE,
-  updated_at DATE
+  created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  updated_at INT
 );
 
 CREATE TABLE rooms (
   id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
   name VARCHAR(32),
-  created_at DATE,
-  updated_at DATE
+  created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  updated_at INT
 );
 
 CREATE TABLE users (
   id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
   name VARCHAR(32),
   post_count INT(8),
-  last_post DATE,
-  created_at DATE,
-  updated_at DATE
+  last_post INT,
+  created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  updated_at INT
 );
 
 
